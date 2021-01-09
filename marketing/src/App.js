028cbda10d346fbf7d83dbc5cedbf2cbbf0d1274
Rehim E. Firudunlu <FirudunluRE@kapitalho.az>
2021-01-09 18:13:30 +0400

    INITIAL COMMIT

---
+++ marketing/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { StylesProvider } from "@material-ui/core/styles";
+
+import Pricing from "./components/Pricing";
+import Landing from "./components/Landing";
+
+const App = () => {
+  return (
+    <StylesProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/pricing" component={Pricing} />
+          <Route path="/" component={Landing} />
+        </Switch>
+      </BrowserRouter>
+    </StylesProvider>
+  );
+};
+
+export default App;
