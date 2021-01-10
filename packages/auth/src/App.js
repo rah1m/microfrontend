3d179de64ff74c0ea698a38d3448e52c764fc15f
Rehim E. Firudunlu <FirudunluRE@kapitalho.az>
2021-01-10 13:42:12 +0400

    some changes

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -12,7 +12,6 @@ const generateClassName = createGenerateClassName({
   productionPrefix: "au",
 });
 const App = ({ history, onSignIn }) => {
-  console.log("");
   return (
     <StylesProvider generateClassName={generateClassName}>
       <Router history={history}>
